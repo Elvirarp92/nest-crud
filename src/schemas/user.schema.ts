b32a32106bdc12d7a33eebcc701521e898fe3f31
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-05 17:25:01 +0200

    make username required for User entries

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,7 +2,7 @@ import * as Mongoose from 'mongoose';
 
 export const UserSchema = new Mongoose.Schema(
   {
-    username: String,
+    username: { type: String, required: true },
     posts: { type: [Mongoose.SchemaTypes.ObjectId], ref: 'Post' },
     favs: { type: [Mongoose.SchemaTypes.ObjectId], ref: 'Post' },
   },
