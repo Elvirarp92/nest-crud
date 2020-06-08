7c8877e219ce67b345bbf8c17c65dc041a2c2395
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 22:58:54 +0200

    import post schema to post module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { PostSchema } from './schemas/post.schema';
 
 @Module({
+
+  imports: [MongooseModule.forFeature([{ name: 'Post', schema: PostSchema }])],
   providers: [PostService],
   controllers: [PostController]
 })
