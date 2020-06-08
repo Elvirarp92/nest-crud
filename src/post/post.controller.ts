0ad9b8d3333fa6c439646a0ac4655a93d0240f93
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 23:07:07 +0200

    create /posts route

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -14,5 +14,11 @@ import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
-  constructor(private postService: PostService) {};
+  constructor(private postService: PostService) {}
+
+  @Get('posts')
+  async getAllPosts(@Res() res) {
+    const allPosts = await this.postService.getAllPosts();
+    return res.status(HttpStatus.OK).json(allPosts);
+  }
 }
