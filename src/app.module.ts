f61e5c587a97a922fc3a162da2632ea1a4727fed
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 21:24:02 +0200

    import PostModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
