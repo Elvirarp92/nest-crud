baa126255a1706c518ce4abc62e015339cacf794
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 23:35:30 +0200

    create newpost route

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,31 +1,41 @@
 import {
   Controller,
   Get,
+  Req,
   Res,
   HttpStatus,
   Body,
-  Put,
+  Post,
   Query,
   NotFoundException,
   Delete,
   Param,
 } from '@nestjs/common';
 import { PostService } from './post.service';
+import { Request, Response } from 'express';
 
 @Controller('post')
 export class PostController {
   constructor(private postService: PostService) {}
 
   @Get('posts')
-  async getAllPosts(@Res() res) {
+  async getAllPosts(@Res() res: Response) {
     const allPosts = await this.postService.getAllPosts();
     return res.status(HttpStatus.OK).json(allPosts);
   }
 
   @Get('post/:postID')
-  async getPost(@Res() res, @Param('postID') postID) {
+  async getPost(@Res() res: Response, @Param('postID') postID) {
     const post = await this.postService.getPost(postID);
     if (!post) throw new NotFoundException('Post does not exist!');
     return res.status(HttpStatus.OK).json(post);
+  }
+
+  @Post('newpost')
+  async createPost(@Req() req: Request, @Res() res: Response, @Body() body) {
+    const newPost = await this.postService.createPost(req.user, body.text);
+    return res.status(HttpStatus.OK).json({
+      message: 'Post created successfully!',
+    });
   }
 }
