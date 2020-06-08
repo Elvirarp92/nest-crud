7d3b7f6c6aff58128f391dd8b0b297ec51be122b
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 22:44:01 +0200

    createPost function ready

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post } from './interfaces/post.interface';
-import { CreatePostDTO } from './dto/create-post.dto';
 
 @Injectable()
 export class PostService {
@@ -18,5 +17,10 @@ export class PostService {
   async getPost(postID: string): Promise<Post> {
     const post = await this.postModel.findById(postID).exec();
     return post;
+  }
+
+  async createPost(author: string, text: string): Promise<Post> {
+    const newPost = await this.postModel.create({ author, text });
+    return newPost.save();
   }
 }
