4c8df6bc8f00a0b5f0d2a977c46e17a1e22f1b68
Elvira Ramírez <elvirarp92@gmail.com>
2020-06-08 21:36:55 +0200

    create Post model

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './interfaces/post.interface';
+import { CreatePostDTO } from './dto/create-post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+
+  constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {}
+  
+}
